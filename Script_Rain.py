1b324fd2cdc88cf6450e56ce7c238bd73e8f5191
JCS <maestronv@gmail.com>
2024-08-24 18:02:13 -0600

    Update Script_Rain.py

--- Script_Rain.py
+++ Script_Rain.py
@@ -1,10 +1,10 @@
 bl_info = {
-    "name": "Water Particles Addon",
+    "name": "Snow/Water Particles Addon",
     "author": "JCS",
     "version": (1, 0),
     "blender": (2, 80, 0),
-    "location": "View3D > Water Droplet Addon",
-    "description": "Adds Rain To Your Model",
+    "location": "View3D > Water Droplet/Snow Addon",
+    "description": "Adds Rain/Snow To Your Model",
     "warning": "",
     "doc_url": "",
     "category": "",
@@ -27,27 +27,32 @@ class ButtonOperator(bpy.types.Operator):
             y = randint (-40, 70)
             z = randint (20, 70)
             bpy.ops.mesh.primitive_ico_sphere_add(enter_editmode=False, align='WORLD', location=(x, y, z), scale=(1, 1, 1))
-            sphere = bpy.context.active_object
-            color = (0.0154895, 0.397459, 0.800505, 1)
-            material = bpy.data.materials.new("material")
+            ico_sphere = bpy.context.active_object
+            color = (0.0154895, 0.397459, 0.800505, 1) 
+            material = bpy.data.materials.new("Color")
             material.diffuse_color = color
-            sphere.data.materials.append(material)
+            mat = bpy.data.materials.new(name='Material')
+            mat.use_nodes=True
+            mat_nodes = mat.node_tree.nodes
+            mat_links = mat.node_tree.links
+            ico_sphere.data.materials.append(mat)        
+            mat_nodes["Principled BSDF"].inputs['Metallic'].default_value = 1
             bpy.ops.object.shade_smooth()
 
 
         return {'FINISHED'}
 class CustomPanel(bpy.types.Panel):
-    bl_label = "Water Droplet Panel"
+    bl_label = "Water Droplet/Snow Panel"
     bl_idname = "OBJECT_PT_random"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = "Water Droplet Addon"
+    bl_category = "Water Droplet/Snow Addon"
 
     def draw(self, context):
         layout = self.layout
         obj = context.object
         row = layout.row()
-        row.operator(ButtonOperator.bl_idname, text="Add Rain", icon='KEYTYPE_BREAKDOWN_VEC')
+        row.operator(ButtonOperator.bl_idname, text="Add Rain/Snow", icon='SHADING_SOLID')
 
 from bpy.utils import register_class, unregister_class
 
